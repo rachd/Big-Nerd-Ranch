a2ce29b0bc636c65c0e907e99c3e73acfda70cef
Rachel Dorn <rachel.m.dorn@gmail.com>
2016-05-16 22:18:58 -0400

    Set up labels and buttons

---
+++ Quiz/RMDQuizViewController.h
@@ -0,0 +1,13 @@
+//
+//  RMDQuizViewController.h
+//  Quiz
+//
+//  Created by Rachel Dorn on 5/16/16.
+//  Copyright © 2016 Rachel Dorn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RMDQuizViewController : UIViewController
+
+@end

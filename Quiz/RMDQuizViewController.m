c34162aaed6d2cd38f8f6c32f86b826373e1734c
Rachel Dorn <rachel.m.dorn@gmail.com>
2016-05-16 22:37:22 -0400

    set up connections

--- Quiz/RMDQuizViewController.m
+++ Quiz/RMDQuizViewController.m
@@ -10,28 +10,19 @@
 
 @interface RMDQuizViewController ()
 
+@property (nonatomic, weak) IBOutlet UILabel *questionLabel;
+@property (nonatomic, weak) IBOutlet UILabel *answerLabel;
+
 @end
 
 @implementation RMDQuizViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+- (IBAction)showQuestion:(id)sender {
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)showAnswer:(id)sender {
+    
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end

58e70da1d3634f6894dd143d6a512c2d3dbffaba
Rachel Dorn <rachel.m.dorn@gmail.com>
2016-05-16 22:59:24 -0400

    add VC methods

--- Quiz/RMDQuizViewController.m
+++ Quiz/RMDQuizViewController.m
@@ -10,6 +10,11 @@
 
 @interface RMDQuizViewController ()
 
+@property (nonatomic) int currentQuestionIndex;
+
+@property (nonatomic, copy) NSArray *questions;
+@property (nonatomic, copy) NSArray *answers;
+
 @property (nonatomic, weak) IBOutlet UILabel *questionLabel;
 @property (nonatomic, weak) IBOutlet UILabel *answerLabel;
 
@@ -17,12 +22,32 @@
 
 @implementation RMDQuizViewController
 
-- (IBAction)showQuestion:(id)sender {
+- (instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     
+    if (self) {
+        self.questions = @[@"From what is cognac made?", @"What is 7+7?", @"What is the capital of Vermont?"];
+        
+        self.answers = @[@"Grapes", @"14", @"Montpelier"];
+    }
+    
+    return self;
+}
+
+- (IBAction)showQuestion:(id)sender {
+    self.currentQuestionIndex++;
+    if (self.currentQuestionIndex == [self.questions count]) {
+        self.currentQuestionIndex = 0;
+    }
+    
+    NSString *question = self.questions[self.currentQuestionIndex];
+    self.questionLabel.text = question;
+    self.answerLabel.text = @"???";
 }
 
 - (IBAction)showAnswer:(id)sender {
-    
+    NSString *answer = self.answers[self.currentQuestionIndex];
+    self.answerLabel.text = answer;
 }
 
 @end

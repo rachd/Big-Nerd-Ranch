ece953b718408fdb9eacc17a9d6b59a6d266e911
Rachel Dorn <rachel.m.dorn@gmail.com>
2016-05-17 21:43:51 -0400

    write setters and getters

---
+++ RandomItems/RandomItems/RMDItem.h
@@ -0,0 +1,29 @@
+//
+//  RMDItem.h
+//  RandomItems
+//
+//  Created by Rachel Dorn on 5/17/16.
+//  Copyright © 2016 Rachel Dorn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RMDItem : NSObject {
+    NSString *_itemName;
+    NSString *_serialNumber;
+    int _valueInDollars;
+    NSDate *_dateCreated;
+}
+
+- (void)setItemName:(NSString *)str;
+- (NSString *)itemName;
+
+- (void)setSerialNumber:(NSString *)str;
+- (NSString *)serialNumber;
+
+- (void)setValueInDollars:(int)v;
+- (int)valueInDollars;
+
+- (NSDate *)dateCreated;
+
+@end
---
+++ RandomItems/RandomItems/RMDItem.m
@@ -0,0 +1,41 @@
+//
+//  RMDItem.m
+//  RandomItems
+//
+//  Created by Rachel Dorn on 5/17/16.
+//  Copyright © 2016 Rachel Dorn. All rights reserved.
+//
+
+#import "RMDItem.h"
+
+@implementation RMDItem
+
+- (void)setItemName:(NSString *)str {
+    _itemName = str;
+}
+
+- (NSString *)itemName {
+    return _itemName;
+}
+
+- (void)setSerialNumber:(NSString *)str {
+    _serialNumber = str;
+}
+
+- (NSString *)serialNumber {
+    return _serialNumber;
+}
+
+- (void)setValueInDollars:(int)v {
+    _valueInDollars = v;
+}
+
+- (int)valueInDollars {
+    return _valueInDollars;
+}
+
+- (NSDate *)dateCreated {
+    return _dateCreated;
+}
+
+@end

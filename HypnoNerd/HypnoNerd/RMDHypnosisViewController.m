f0b3a7b27559cb2f7dadf4f0fee23d366971cbe4
Rachel Dorn <rachel.m.dorn@gmail.com>
2016-05-24 21:09:19 -0400

    add 20 uilabels

--- HypnoNerd/HypnoNerd/RMDHypnosisViewController.m
+++ HypnoNerd/HypnoNerd/RMDHypnosisViewController.m
@@ -9,6 +9,10 @@
 #import "RMDHypnosisViewController.h"
 #import "RMDHypnosisView.h"
 
+@interface RMDHypnosisViewController () <UITextFieldDelegate>
+
+@end
+
 @implementation RMDHypnosisViewController
 
 - (instancetype)init {
@@ -24,7 +28,44 @@
 - (void)loadView {
     CGRect frame = [UIScreen mainScreen].bounds;
     RMDHypnosisView *backgroundView = [[RMDHypnosisView alloc] initWithFrame:frame];
+    
+    UITextField *textField = [[UITextField alloc] initWithFrame:CGRectMake(40, 70, 240, 30)];
+    textField.borderStyle = UITextBorderStyleRoundedRect;
+    textField.placeholder = @"Hypnotize me";
+    textField.returnKeyType = UIReturnKeyDone;
+    textField.delegate = self;
+    [backgroundView addSubview:textField];
+    
     self.view = backgroundView;
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    [self drawHypnoticMessage:textField.text];
+    textField.text = @"";
+    [textField resignFirstResponder];
+    return YES;
+}
+
+- (void)drawHypnoticMessage:(NSString *)message {
+    for (int i = 0; i < 20; i++) {
+        UILabel *messageLabel = [[UILabel alloc] init];
+        messageLabel.backgroundColor = [UIColor clearColor];
+        messageLabel.textColor = [UIColor whiteColor];
+        messageLabel.text = message;
+        [messageLabel sizeToFit];
+        
+        int width = self.view.bounds.size.width - messageLabel.bounds.size.width;
+        int x = arc4random() % width;
+        
+        int height = self.view.bounds.size.height - messageLabel.bounds.size.height;
+        int y = arc4random() % height;
+        
+        CGRect frame = messageLabel.frame;
+        frame.origin = CGPointMake(x, y);
+        messageLabel.frame = frame;
+        
+        [self.view addSubview:messageLabel];
+    }
 }
 
 @end

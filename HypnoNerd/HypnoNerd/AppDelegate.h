cddd487175636532ff5b45302a7f7a155a621981
Rachel Dorn <rachel.m.dorn@gmail.com>
2016-06-10 18:45:23 -0400

    universalize quiz

---
+++ HypnoNerd/HypnoNerd/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HypnoNerd
+//
+//  Created by Rachel Dorn on 5/22/16.
+//  Copyright © 2016 Rachel Dorn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Quiz/Quiz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Quiz
+//
+//  Created by Rachel Dorn on 5/16/16.
+//  Copyright © 2016 Rachel Dorn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

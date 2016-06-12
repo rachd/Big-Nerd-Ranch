b4f3913d98f271ce050d40dee5707872f1217b4d
Rachel Dorn <rachel.m.dorn@gmail.com>
2016-06-11 23:27:14 -0400

    save images to file

--- Homepwner/Homepwner/RMDImageStore.m
+++ Homepwner/Homepwner/RMDImageStore.m
@@ -40,14 +40,35 @@
 
 - (void)setImage:(UIImage *)image forKey:(NSString *)key {
     self.dictionary[key] = image;
+    NSString *imagePath = [self imagePathForKey:key];
+    NSData *data = UIImageJPEGRepresentation(image, 0.5);
+    [data writeToFile:imagePath atomically:YES];
 }
 
 - (UIImage *)imageForKey:(NSString *)key {
-    return self.dictionary[key];
+    UIImage *result = self.dictionary[key];
+    if (!result) {
+        NSString *imagePath = [self imagePathForKey:key];
+        result = [UIImage imageWithContentsOfFile:imagePath];
+        if (result) {
+            self.dictionary[key] = result;
+        } else {
+            NSLog(@"Error: unable to find %@", imagePath);
+        }
+    }
+    return result;
 }
 
 - (void)deleteImageForKey:(NSString *)key {
     [self.dictionary removeObjectForKey:key];
+    
+    NSString *imagePath = [self imagePathForKey:key];
+    [[NSFileManager defaultManager] removeItemAtPath:imagePath error:nil];
 }
 
+- (NSString *)imagePathForKey:(NSString *)key {
+    NSArray *documentDirectories = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentDirectory = [documentDirectories firstObject];
+    return [documentDirectory stringByAppendingPathComponent:key];
+}
 @end
